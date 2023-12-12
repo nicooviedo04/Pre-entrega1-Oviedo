57430b2da97935d0939e94cf6a848b6634818661
Nicolas <Nicoovi.04@gmail.com>
2023-12-11 23:52:04 -0300

    se agrego descripcion a los productos

--- src/Component/Cartwidget/CartElement.jsx
+++ src/Component/Cartwidget/CartElement.jsx
@@ -1,20 +1,19 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { dataContext } from "../Context/DataContext";
-import React from 'react';
 
 const CartElement = () => {
-    const { cart } = useContext(dataContext);
-  return  cart.map((product) => {
-    return(
+  const { cart } = useContext(dataContext);
 
-        <div className="cartContent" key={product.id}>
-        <img src={product.img} alt="nft-card" />
-        <h3 className="name">{product.name}</h3>
+  return cart.map((product) => (
+    <div className="cartContent" key={product.id}>
+      <img className="cartImage" src={product.img} alt="nft-card" />
+      <div className="descriptionContainer">
+        <h3 className="title">{product.name}</h3>
+        <h4 className="description">{product.description}</h4>
         <h4 className="price">${product.price}</h4>
-        
       </div>
-          )
-        })
-}
+    </div>
+  ));
+};
 
 export default CartElement;
